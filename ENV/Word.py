1eeff40438e534e0c1dec3663caba32937468895
Tsachi Kotek <tkotek@allot.com>
2019-07-03 20:55:45 +0300

    Adding all files to repository

--- ENV/Word.py
+++
@@ -1,9 +0,0 @@
-import docx
-from document import Document
-
-print ('Documenting...')
-document = Document()
-document.add_heading('Document Title', 0)
-
-print ('Saving...')
-document.save('test.docx')
